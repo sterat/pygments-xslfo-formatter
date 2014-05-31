9d0ff698b931a7805ddec6751909311789e2b563
Steve Ratcliffe <sr@parabola.me.uk>
2014-05-31 22:11:51 +0100

    Add initial version, working with asciidoc.

---
+++ pygments_xslfo_formatter/xslfo.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+	pygments.formatters.html
+	~~~~~~~~~~~~~~~~~~~~~~~~
+
+	Formatter for XSL-FO output.
+
+	:copyright: Copyright 2014, Steve Ratcliffe
+	:license: BSD, see LICENSE for details.
+"""
+
+from pygments.formatter import Formatter
+from pygments.util import bytes
+
+__all__ = ['XslfoFormatter']
+
+
+_escape_html_table = {
+	ord('&'): u'&amp;',
+	ord('<'): u'&lt;',
+	ord('>'): u'&gt;',
+	ord('"'): u'&quot;',
+	ord("'"): u'&#39;',
+	}
+
+
+def escape_html(text, table=_escape_html_table):
+	"""Escape &, <, > as well as single and double quotes for HTML."""
+	return text.translate(table)
+
+
+class XslfoFormatter(Formatter):
+	r"""
+	Format tokens as XSL-FO ``<fo:inline>`` tags.
+	"""
+
+	name = 'XSL-FO formatter'
+	aliases = ['xslfo', 'xsl-fo']
+	filenames = []
+
+	def __init__(self, **options):
+		Formatter.__init__(self, **options)
+		self.title = self._decodeifneeded(self.title)
+
+		# Holds fo attributes that correspond to each style type.
+		self.styles = {}
+
+		# Iterate over the style and create a list of fo attributes that describe it.
+		for token, style in self.style:
+			atts = []
+			if style['color']:
+				atts.append('color="#%s"' % style['color'])
+			if style['bold']:
+				atts.append('font-weight="bold"')
+			if style['italic']:
+				atts.append('font-style="italic"')
+			if style['underline']:
+				atts.append('text-decoration="underline"')
+
+			# Save the attributes for this style
+			self.styles[token] = ' '.join(atts)
+
+	def _decodeifneeded(self, value):
+		if isinstance(value, bytes):
+			if self.encoding:
+				return value.decode(self.encoding)
+			return value.decode()
+		return value
+
+	def format_unencoded(self, tokensource, outfile):
+		"""
+		Read the token source and write it marked up with fo:inline styles.
+
+		We join together tokens with the same token type and then surround the
+		text with a fo:inline tag with appropriate attributes for the style
+		element.
+
+		The whole thing is wrapped in a fo:block tag.
+		"""
+
+		def write_value(t, val):
+			atts = self.styles[t]
+			outfile.write('<fo:inline %s>%s</fo:inline>' % (atts, escape_html(val)))
+
+		outfile.write('<fo:block xmlns:fo="http://www.w3.org/1999/XSL/Format">')
+
+		lastval = ''
+		lasttype = None
+		for ttype, value in tokensource:
+			while ttype not in self.styles:
+				ttype = ttype.parent
+
+			if ttype == lasttype:
+				lastval += value
+			else:
+				if lastval:
+					write_value(lasttype, lastval)
+				lastval = value
+				lasttype = ttype
+
+		if lastval:
+			write_value(lasttype, lastval)
+
+		outfile.write('</fo:block>')
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from os import path
+from codecs import open
+from setuptools import setup
+
+# Get the long description
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(name='pygments-xslfo-formatter',
+	version='1.0',
+	description='A pygments formatter that outputs <fo:inline> tags',
+	long_description=long_description,
+	keywords='syntax highlighting xsl-fo',
+	author='Steve Ratcliffe',
+	author_email='sr@parabola.me.uk',
+	url='https://bitbucket.org/sratcliffe/pygments-xslfo-formatter',
+	license='MIT',
+	platforms='any',
+
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		#'Development Status :: 5 - Production/Stable',
+
+		'Intended Audience :: Developers',
+		'Intended Audience :: End Users/Desktop',
+
+		# Pick your license as you wish (should match "license" above)
+		'License :: OSI Approved :: MIT License',
+
+		# Specify the Python versions you support here. In particular, ensure
+		# that you indicate whether you support Python 2, Python 3 or both.
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+
+		'Topic :: Text Processing :: Filters',
+	],
+
+	packages=['pygments_xslfo_formatter'],
+
+	entry_points={
+		'pygments.formatters': ['xslfo = pygments_xslfo_formatter.xslfo:XslfoFormatter']
+	},
+
+	install_requires=['Pygments'],
+)
